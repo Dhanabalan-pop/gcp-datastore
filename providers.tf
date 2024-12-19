fedc6dac27fe827e1583466556658a644dc7800d
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2024-12-19 20:05:56 +0530

    Added service account key and project details

--- providers.tf
+++ providers.tf
@@ -1,5 +1,13 @@
-  resource "null_resource" "example" {
-    provisioner "local-exec" {
-      command = "echo This command will execute whenever the configuration changes"
-    }
-  }
+# Configure the Google Cloud provider
+provider "google" {
+  credentials = file("arctic-marking-445204-v5-fadc02926f28")  # Path to your service account key file
+  project     = "arctic-marking-445204-v5"  # Your Google Cloud project ID
+  region      = "us-central1"        # The region you want to use (optional, but good practice)
+}
+
+# Example resource - Create a Google Cloud Storage bucket
+resource "google_storage_bucket" "bucket" {
+  name          = "my-tf-bucket"
+  location      = "US"
+  force_destroy = true
+}
