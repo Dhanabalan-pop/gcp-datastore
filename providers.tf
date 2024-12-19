096c5ebf5e65b4aa5efebbb57b43597f87ad22de
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2024-12-19 22:36:22 +0530

    service account

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 # Configure the Google Cloud provider
 provider "google" {
-  credentials = var.service_account  # Path to your service account key file
+  credentials = jsondecode(base64decode(var.GOOGLE_CREDENTIALS_BASE64))  # Path to your service account key file
   project     = "arctic-marking-445204-v5"  # Your Google Cloud project ID
   region      = "us-central1"        # The region you want to use (optional, but good practice)
 }
@@ -12,6 +12,6 @@ resource "google_storage_bucket" "bucket" {
   force_destroy = true
 }
 
-variable service_account {
+variable GOOGLE_CREDENTIALS_BASE64 {
   type = string
 }
