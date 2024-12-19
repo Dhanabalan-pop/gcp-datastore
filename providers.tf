c8e9e8b87371992abd104822bd756e64ed543840
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2024-12-19 22:21:49 +0530

    Updated the service account variable

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 # Configure the Google Cloud provider
 provider "google" {
-  credentials = file("arctic-marking-445204-v5-fadc02926f28.json")  # Path to your service account key file
+  credentials = var.service_account  # Path to your service account key file
   project     = "arctic-marking-445204-v5"  # Your Google Cloud project ID
   region      = "us-central1"        # The region you want to use (optional, but good practice)
 }
@@ -10,4 +10,9 @@ resource "google_storage_bucket" "bucket" {
   name          = "my-tf-bucket"
   location      = "US"
   force_destroy = true
+}
+
+variable service_account {
+  type = "string"
+  sensitive = true
 }
